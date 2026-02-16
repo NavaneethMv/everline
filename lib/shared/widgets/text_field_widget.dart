943f07545083f8894402c7d03d7af7a6ede05a7b
NavaneethMv <mvnavaneeth5@gmail.com>
2026-02-16 17:17:03 +0530

    feat: Update TextFieldWidget to support optional label and additional properties

--- lib/shared/widgets/text_field_widget.dart
+++ lib/shared/widgets/text_field_widget.dart
@@ -3,18 +3,22 @@ import 'package:shadcn_ui/shadcn_ui.dart';
 
 class TextFieldWidget extends StatelessWidget {
   final bool? requiredMark;
-  final String label;
+  final String? label;
   final String placeholder;
   final void Function(String) onChanged;
   final String? error;
   final TextInputType? keyboardType;
   final String? initialValue;
+  final Widget? leading;
+  final FocusNode? focusNode;
+  final bool obscureText;
+  final Color? fillColor;
 
   final EdgeInsetsGeometry? padding;
 
   const TextFieldWidget({
     super.key,
-    required this.label,
+    this.label,
     required this.placeholder,
     required this.onChanged,
     this.error,
@@ -22,6 +26,10 @@ class TextFieldWidget extends StatelessWidget {
     this.requiredMark = false,
     this.padding,
     this.initialValue,
+    this.leading,
+    this.focusNode,
+    this.obscureText = false,
+    this.fillColor,
   });
 
   @override
@@ -31,30 +39,37 @@ class TextFieldWidget extends StatelessWidget {
       spacing: 8.0,
       children: [
         ShadInputFormField(
+          focusNode: focusNode,
+          obscureText: obscureText,
           decoration: ShadDecoration(
-            color: Colors.grey[50],
+            color: fillColor ?? Colors.grey[50],
             border: ShadBorder.all(
               width: 1,
               radius: const BorderRadius.all(Radius.circular(8)),
             ),
           ),
           padding: padding,
-          label: requiredMark == true
-              ? RichText(
-                  text: TextSpan(
-                    text: label,
-                    style: DefaultTextStyle.of(context).style,
-                    children: [
-                      TextSpan(
-                        text: ' *',
-                        style: TextStyle(
-                          color: ShadTheme.of(context).colorScheme.destructive,
+          leading: leading,
+          label: label != null
+              ? (requiredMark == true
+                    ? RichText(
+                        text: TextSpan(
+                          text: label,
+                          style: DefaultTextStyle.of(context).style,
+                          children: [
+                            TextSpan(
+                              text: ' *',
+                              style: TextStyle(
+                                color: ShadTheme.of(
+                                  context,
+                                ).colorScheme.destructive,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
-                  ),
-                )
-              : Text(label),
+                      )
+                    : Text(label!))
+              : null,
           placeholder: Text(placeholder),
           onChanged: onChanged,
           keyboardType: keyboardType,
