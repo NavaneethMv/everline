e50b8b9e574753bb83ffa536d5dc11e1bafc9862
NavaneethMv <mvnavaneeth5@gmail.com>
2026-02-16 17:13:49 +0530

    feat: Implement logout functionality and terms acceptance handling in AuthBloc

--- lib/features/auth/bloc/auth_bloc.dart
+++ lib/features/auth/bloc/auth_bloc.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:everline/core/service_locator.dart';
@@ -14,7 +16,20 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthState()) {
     on<EmailChangedEvent>(_emailChanged);
     on<PassWordChangedEvent>(_passwordChanged);
+    on<TermsAcceptedChangedEvent>(_termsAcceptedChanged);
+    on<LoggedOutEvent>(_loggedOut);
     on<LoginEvent>(_login);
+  }
+
+  Future<void> _loggedOut(LoggedOutEvent event, Emitter<AuthState> emit) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.clear();
+      await _authRepository.signOut();
+      emit(const AuthState());
+    } catch (_) {
+      emit(state.copyWith(status: AuthStatus.failure));
+    }
   }
 
   FutureOr<void> _emailChanged(
@@ -31,7 +46,23 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     emit(state.copyWith(password: event.password));
   }
 
+  FutureOr<void> _termsAcceptedChanged(
+    TermsAcceptedChangedEvent event,
+    Emitter<AuthState> emit,
+  ) {
+    emit(state.copyWith(isTermsAccepted: event.isTermsAccepted));
+  }
+
   Future<void> _login(LoginEvent event, Emitter<AuthState> emit) async {
+    if (!state.isTermsAccepted) {
+      emit(
+        state.copyWith(
+          status: AuthStatus.failure,
+          errorMessage: 'Please accept terms and conditions',
+        ),
+      );
+      return;
+    }
     emit(state.copyWith(status: AuthStatus.loading));
     try {
       final response = await _authRepository.signInWithEmail(
