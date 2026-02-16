e67786a1b7aaf79e27e1edbb2155904dc6fbee52
NavaneethMv <mvnavaneeth5@gmail.com>
2026-02-16 17:14:44 +0530

    feat: Refactor MembersView to use StatefulWidget and enhance search functionality

--- lib/features/members/views/members_view.dart
+++ lib/features/members/views/members_view.dart
@@ -1,13 +1,44 @@
 import 'package:everline/features/members/bloc/members_cubit.dart';
 import 'package:everline/features/tree/repository/tree_repository.dart';
+import 'package:everline/shared/widgets/common_app_bar.dart';
 import 'package:everline/shared/widgets/people_card_widget.dart';
+import 'package:everline/shared/widgets/text_field_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
-class MembersView extends StatelessWidget {
+class MembersView extends StatefulWidget {
   const MembersView({super.key});
+
+  @override
+  State<MembersView> createState() => _MembersViewState();
+}
+
+class _MembersViewState extends State<MembersView> {
+  final FocusNode _searchFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    // Check if we need to focus the search field based on navigation extras
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final state = GoRouterState.of(context);
+      if (state.extra != null &&
+          state.extra is Map &&
+          (state.extra as Map)['focus'] == true) {
+        _searchFocusNode.requestFocus();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +46,7 @@ class MembersView extends StatelessWidget {
       create: (context) =>
           MembersCubit(repository: TreeRepository())..loadMembers(),
       child: Scaffold(
+        appBar: const CommonAppBar(title: 'Family Members', showSearch: false),
         backgroundColor: ShadTheme.of(context).colorScheme.background,
         body: SafeArea(
           child: Padding(
@@ -22,12 +54,7 @@ class MembersView extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Family Members',
-                  style: ShadTheme.of(context).textTheme.h3,
-                ),
-                const SizedBox(height: 16),
-                _SearchField(),
+                _SearchField(focusNode: _searchFocusNode),
                 const SizedBox(height: 16),
                 Expanded(
                   child: BlocBuilder<MembersCubit, MembersState>(
@@ -44,32 +71,55 @@ class MembersView extends StatelessWidget {
                                 (_) => (null as dynamic), // Dummy for skeleton
                               );
 
-                        return Skeletonizer(
-                          enabled: state is MembersLoading,
-                          child: ListView.separated(
-                            itemCount: state is MembersLoading
-                                ? 10
-                                : members.length,
-                            separatorBuilder: (context, index) =>
-                                const SizedBox(height: 12),
-                            itemBuilder: (context, index) {
-                              if (state is MembersLoading) {
-                                return const PeopleCardWidget(
-                                  name: 'Loading Name',
-                                  role: 'Family Member',
-                                  avatarUrl: '',
-                                );
-                              }
+                        return members.isEmpty
+                            ? Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SizedBox(
+                                      height: 100,
+                                      width: 100,
+                                      child: Lottie.asset(
+                                        'assets/animations/cat-animation.json',
+                                      ),
+                                    ),
+                                    Text(
+                                      "Your family tree is feeling lonely! :(",
+                                      style: Theme.of(
+                                        context,
+                                      ).textTheme.bodySmall,
+                                    ),
+                                  ],
+                                ),
+                              )
+                            : Skeletonizer(
+                                enabled: state is MembersLoading,
+                                child: ListView.separated(
+                                  itemCount: state is MembersLoading
+                                      ? 10
+                                      : members.length,
+                                  separatorBuilder: (context, index) =>
+                                      const SizedBox(height: 12),
+                                  itemBuilder: (context, index) {
+                                    if (state is MembersLoading) {
+                                      return const PeopleCardWidget(
+                                        name: 'Loading Name',
+                                        role: 'Family Member',
+                                        avatarUrl: '',
+                                      );
+                                    }
 
-                              final member = members[index];
-                              return PeopleCardWidget(
-                                name: '${member.firstName} ${member.lastName}',
-                                role: member.occupation ?? 'Family Member',
-                                avatarUrl: member.profileImageUrl,
+                                    final member = members[index];
+                                    return PeopleCardWidget(
+                                      name:
+                                          '${member.firstName} ${member.lastName}',
+                                      role:
+                                          member.occupation ?? 'Family Member',
+                                      avatarUrl: member.profileImageUrl,
+                                    );
+                                  },
+                                ),
                               );
-                            },
-                          ),
-                        );
                       }
 
                       return const Center(child: CircularProgressIndicator());
@@ -86,16 +136,25 @@ class MembersView extends StatelessWidget {
 }
 
 class _SearchField extends StatelessWidget {
+  final FocusNode? focusNode;
+
+  const _SearchField({this.focusNode});
+
   @override
   Widget build(BuildContext context) {
-    return ShadInput(
-      placeholder: const Text('Search members...'),
-      onChanged: (value) {
-        context.read<MembersCubit>().searchMembers(value);
-      },
-      leading: const Padding(
-        padding: EdgeInsets.all(4.0),
-        child: Icon(LucideIcons.search, size: 16),
+    return Hero(
+      tag: 'members_search_field',
+      child: TextFieldWidget(
+        placeholder: 'Search members...',
+        padding: const EdgeInsets.all(16),
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: Icon(LucideIcons.search, size: 16),
+        ),
+        onChanged: (value) {
+          context.read<MembersCubit>().searchMembers(value);
+        },
+        focusNode: focusNode,
       ),
     );
   }
